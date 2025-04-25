0318881c2028a9f646b7c2f2854b619bd8a059ec
ArdaCenker <arda.cenker@marun.edu.tr>
2025-04-25 18:28:04 +0300

    add basic enemy features

--- src/enemy/Enemy.java
+++ src/enemy/Enemy.java
@@ -1,5 +1,18 @@
 package enemy;
 
 public class Enemy {
+	
+	private int health;
+	
+	public Enemy(int health) {
+		this.health = health;
+	}
 
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
 }

4e87b8d5c3c2ba258e5f30f4ca48e4f794e567ed
tzencirkiran <talhazencirkiran@marun.edu.tr>
2025-04-28 18:36:37 +0300

    Some fixes in findPath method

--- src/enemy/Enemy.java
+++ src/enemy/Enemy.java
@@ -12,7 +12,10 @@ public class Enemy {
 	private float location_x;
 	private float location_y;
 
+	// TODO: Build a constructor
+
 	// r_1(x1,y1), r_2(x2,y2)... Rows for targeted_locations
+	// locations will be provided by the LevelManager class
 	ArrayList<float[][]> targeted_locations;
 	
 	public Enemy(int health) {
@@ -34,6 +37,8 @@ public class Enemy {
 		// Assigns first target loc's x and y values to new variables
 		float target_loc_x = targeted_locations.getFirst()[0][0];
 		float target_loc_y = targeted_locations.getFirst()[1][0];
+		// Creates an array for storing direction
+		float[] direction = new float[2];
 
 		// Checks whether the object arrived to first target destination
 		if (target_loc_x == location_x && target_loc_y == location_y)
@@ -45,13 +50,18 @@ public class Enemy {
 			target_loc_y = targeted_locations.getFirst()[1][0];
 		}
 
-		// set path as hypotenuse of two locations, and assign tangent to tangent
-		float tangent = (target_loc_y - location_y) / (target_loc_x - location_x);
-
-		// direction will hold the ratio, it's like a vector for speed
-		float[] direction = new float[2];
-		direction[0] = 1;
-		direction[1] = tangent;
+		// Sets direction of enemy according to enemy's and target's location
+		if ((target_loc_x - location_x) != 0){
+			// set path as hypotenuse of two locations, and assign tangent to tangent
+			float tangent = (target_loc_y - location_y) / (target_loc_x - location_x);
+			direction[0] = 1;
+			direction[1] = tangent;
+		}
+		// if divisor value of tangent becomes equivalent to 0
+		else if ((target_loc_x - location_x) == 0) {
+			direction[0] = 0;
+			direction[1] = ((target_loc_y - location_y) > 0) ? 1 : -1;
+		}
 
 		// return direction
 		return direction;

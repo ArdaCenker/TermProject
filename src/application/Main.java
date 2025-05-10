d7443be1ab874d8343b0203b7f1198ceff79d99f
ArdaCenker <arda.cenker@marun.edu.tr>
2025-05-10 18:56:07 +0300

    some minor changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import level.Level;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -12,7 +13,9 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
+			Level level1 = new Level("level1.txt");
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			root.getChildren().addAll(level1.drawLevel());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
